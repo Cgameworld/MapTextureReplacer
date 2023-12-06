155a9bf4e8b439a21bec7aaa14199fd9b842d94c
Cgameworld <support@cgameworld.com>
2023-12-05 18:59:53 -0800

    buttons added

--- ui_src/counter.jsx
+++ ui_src/counter.jsx
@@ -9,21 +9,38 @@ const $Counter = ({react}) => {
     useDataUpdate(react, 'vehicle_counter.current_vehicle_count', setCurrentCount)
 
     // Below, engine.trigger is responsible for triggering the TriggerBinding in the UI System
-    return <$Panel react={react} title="Map Texture">
+    return <$Panel react={react} title="Map Texture Replacer" style="height:430rem">
         <div className="field_MBO">
-            <div className="label_DGc label_ZLb">Active vehicles</div>
-            <div>{currentCount}</div>
+            <div className="label_DGc label_ZLb">Grass Diffuse</div>
+            <button className="button_WWa button_SH8" onClick={() => engine.trigger('vehicle_counter.remove_vehicles')}>Select Image</button>
         </div>
-        <div>
-            <button className="button_WWa button_SH8" onClick={() => engine.trigger('vehicle_counter.remove_vehicles')}>Remove vehicles</button>
+        <div className="field_MBO">
+            <div className="label_DGc label_ZLb">Grass Normal</div>
+            <button className="button_WWa button_SH8" onClick={() => engine.trigger('vehicle_counter.remove_vehicles')}>Select Image</button>
+        </div>
+        <div className="field_MBO">
+            <div className="label_DGc label_ZLb">Dirt Diffuse</div>
+            <button className="button_WWa button_SH8" onClick={() => engine.trigger('vehicle_counter.remove_vehicles')}>Select Image</button>
+        </div>
+        <div className="field_MBO">
+            <div className="label_DGc label_ZLb">Dirt Normal</div>
+            <button className="button_WWa button_SH8" onClick={() => engine.trigger('vehicle_counter.remove_vehicles')}>Select Image</button>
+        </div>
+        <div className="field_MBO">
+            <div className="label_DGc label_ZLb">Cliff Diffuse</div>
+            <button className="button_WWa button_SH8" onClick={() => engine.trigger('vehicle_counter.remove_vehicles')}>Select Image</button>
+        </div>
+        <div className="field_MBO">
+            <div className="label_DGc label_ZLb">Cliff Normal</div>
+            <button className="button_WWa button_SH8" onClick={() => engine.trigger('vehicle_counter.remove_vehicles')}>Select Image</button>
         </div>
     </$Panel>
 }
 
-/ Registering the panel with HookUI so it shows up in the menu
+//Registering the panel with HookUI so it shows up in the menu
 window._$hookui.registerPanel({
     id: "example.map_texture",
-    name: "Map Texture",
+    name: "Map Texture Replacer",
     icon: "Media/Game/Icons/MapTile.svg",
     component: $Counter
 })
