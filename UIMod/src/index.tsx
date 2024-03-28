92cfaf664457c5df66c1d585bb577f96ec8be3b0
Cgameworld <support@cgameworld.com>
2024-03-27 21:31:00 -0700

    mod icon resize bug fixed

--- UIMod/src/index.tsx
+++ UIMod/src/index.tsx
@@ -6,7 +6,7 @@ const register: ModRegistrar = (moduleRegistry) => {
     const CustomMenuButton = () => {
         return <div>
             <button id="MapTextureReplacer-MainGameButton" className="button_ke4 button_ke4 button_h9N" onClick={() => trigger("map_texture", "MainWindowCreate")}>
-                <div className="tinted-icon_iKo icon_be5" style={{ backgroundImage: 'url(Media/Game/Icons/MapTile.svg)', backgroundPositionY: '-1rem', backgroundColor: 'rgba(255,255,255,0)' }}>
+                <div className="tinted-icon_iKo icon_be5" style={{ backgroundImage: 'url(Media/Game/Icons/MapTile.svg)', backgroundPositionY: '-1rem', backgroundColor: 'rgba(255,255,255,0)', backgroundSize: '40rem 40rem' }}>
                 </div>
             </button>
         </div>;
