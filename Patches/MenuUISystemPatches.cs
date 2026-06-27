0f20e9b437bb158118e9fe566d15e97475e5bc06
Cgameworld <support@cgameworld.com>
2026-06-27 00:31:00 -0700

    updated description/images

--- Patches/MenuUISystemPatches.cs
+++ Patches/MenuUISystemPatches.cs
@@ -4,20 +4,23 @@ using MapTextureReplacer;
 using MapTextureReplacer.Systems;
 using Unity.Entities;
 
-[HarmonyPatch(typeof(MenuUISystem), "ExitToMainMenu")]
-public static class MenuUISystem_ExitToMainMenu_Prefix
+namespace MapTextureReplacer.Patches
 {
-    static void Prefix(MenuUISystem __instance)
+    [HarmonyPatch(typeof(MenuUISystem), "ExitToMainMenu")]
+    public static class MenuUISystem_ExitToMainMenu_Prefix
     {
-        var sys = World.DefaultGameObjectInjectionWorld?.GetOrCreateSystemManaged<MapTextureReplacerSystem>();
-        var ui = World.DefaultGameObjectInjectionWorld?.GetOrCreateSystemManaged<MapTextureReplacerUISystem>();
+        static void Prefix(MenuUISystem __instance)
+        {
+            var sys = World.DefaultGameObjectInjectionWorld?.GetOrCreateSystemManaged<MapTextureReplacerSystem>();
+            var ui = World.DefaultGameObjectInjectionWorld?.GetOrCreateSystemManaged<MapTextureReplacerUISystem>();
 
-        ui?.SetWindowOpen(false);
+            ui?.SetWindowOpen(false);
 
-        //drop overrides + restore pristine tiling so the shared prefab doesn't leak into the next map;
-        //saved selections/tiling reapply on the next load (MapTextureReplacerInGameLoadedSystem)
-        sys?.OnExitToMenu();
+            //drop overrides + restore pristine tiling so the shared prefab doesn't leak into the next map;
+            //saved selections/tiling reapply on the next load (MapTextureReplacerInGameLoadedSystem)
+            sys?.OnExitToMenu();
 
-        Mod.log.Info("Restored vanilla terrain on exit to menu");
+            Mod.log.Info("Restored vanilla terrain on exit to menu");
+        }
     }
 }
